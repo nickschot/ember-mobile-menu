0de4c3f79e9fb44d1606a648731948fb5cd3ffe2
nickschot <nickschot@gmail.com>
2020-10-29 16:56:44 +0100

    properly cleanup body-scroll-lock when the tray component is destroyed

--- tests/integration/components/mobile-menu/tray-test.js
+++ tests/integration/components/mobile-menu/tray-test.js
@@ -7,9 +7,6 @@ module('Integration | Component | mobile-menu/tray', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<MobileMenu::Tray/>`);
 
     assert.equal(this.element.textContent.trim(), '');
@@ -22,5 +19,25 @@ module('Integration | Component | mobile-menu/tray', function(hooks) {
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
+  test('it cleans up body-scroll-lock after undrendering', async function(assert) {
+    this.set('showMenu', true);
+    this.set('isClosed', true);
+
+    await render(hbs`{{#if this.showMenu}}<MobileMenu::Tray @preventScroll={{true}} @embed={{false}} @isClosed={{this.isClosed}}/>{{/if}}`);
+    assert.equal(document.body.style.overflow, '');
+
+    this.set('isClosed', false);
+    assert.equal(document.body.style.overflow, 'hidden');
+
+    this.set('isClosed', true);
+    assert.equal(document.body.style.overflow, '');
+
+    this.set('isClosed', false);
+    assert.equal(document.body.style.overflow, 'hidden');
+
+    this.set('showMenu', false);
+    assert.equal(document.body.style.overflow, '');
   });
 });
