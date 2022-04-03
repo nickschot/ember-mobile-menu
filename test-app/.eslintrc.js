71df2a7f2e47aade253e4d0b0071d70f419eeeb6
nickschot <nickschot@gmail.com>
2022-04-03 15:37:01 +0200

    Migrate to monorepo

--- test-app/.eslintrc.js
+++ test-app/.eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
         './testem.js',
         './blueprints/*/index.js',
         './config/**/*.js',
-        './tests/dummy/config/**/*.js',
+        './tests/test-app/config/**/*.js',
         './compile-css.js',
       ],
       parserOptions: {
