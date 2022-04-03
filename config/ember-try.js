57a9e214ef325b8b7a0da860ae2a72189a2571e3
nickschot <nickschot@gmail.com>
2022-04-03 11:38:07 +0200

    Drop Ember 3.23 support

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,14 +8,6 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-3.23',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.23.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
