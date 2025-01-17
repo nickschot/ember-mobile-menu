49c69e7a14d0a143cfd6e60e9240c2900392b19c
nickschot <nickschot@gmail.com>
2025-01-17 13:57:38 +0100

    Lower ember-resolver to v11 for test-app

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -13,6 +13,7 @@ module.exports = async function () {
           devDependencies: {
             'ember-cli': '~4.12.0',
             'ember-source': '~3.28.0',
+            'ember-resolver': '11.0.1',
           },
         },
       },
@@ -21,6 +22,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.4.0',
+            'ember-resolver': '11.0.1',
           },
         },
       },
@@ -29,6 +31,7 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0',
+            'ember-resolver': '11.0.1',
           },
         },
       },
