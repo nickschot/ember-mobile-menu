8c1d95a683579355bcfe38c700115daddaa7ee33
nickschot <nickschot@gmail.com>
2025-01-17 13:47:18 +0100

    Update test-app

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -3,21 +3,12 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {});
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/test-app`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
+  let app = new EmberApp(defaults, {
+    autoImport: {
+      watchDependencies: ['ember-mobile-menu'],
+    },
+  });
 
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app, {
-    skipBabel: [
-      {
-        package: 'qunit',
-      },
-    ],
-  });
+  return maybeEmbroider(app);
 };
