8714661ca3198f1a0daacd8046ee43481ed71b10
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-08 15:51:12 -0500

    Vite app isn't so complicated

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -11,48 +11,8 @@ module.exports = async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
 
   let app = new EmberApp(defaults, {
-    ...(needsOwnerPolyfill
-      ? {
-          babel: {
-            plugins: [
-              [
-                require.resolve(
-                  'babel-plugin-ember-polyfill-get-and-set-owner-from-ember-owner',
-                ),
-                {},
-              ],
-            ],
-          },
-        }
-      : {}),
     autoImport: {
       watchDependencies: ['ember-mobile-menu'],
-      ...(needsOwnerPolyfill
-        ? {
-            webpack: {
-              module: {
-                rules: [
-                  // transforms v2 addon code
-                  {
-                    test: (filename) => {
-                      return filename.endsWith('.js');
-                    },
-                    use: {
-                      loader: 'babel-loader-8',
-                      options: {
-                        plugins: [
-                          require.resolve(
-                            'babel-plugin-ember-polyfill-get-and-set-owner-from-ember-owner',
-                          ),
-                        ],
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-          }
-        : {}),
     },
   });
 
