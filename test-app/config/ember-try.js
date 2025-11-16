70ec9943d153debff61b1013d4a031479f092168
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-16 15:02:55 -0500

    Add 6.4 and 6.8 to LTS testing

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -86,6 +86,22 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-6.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-6.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.8.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
